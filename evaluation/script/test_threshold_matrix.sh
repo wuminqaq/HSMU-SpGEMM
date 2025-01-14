904a2058539c2fe2dda536a9648ade75f59a7639
wuminqaq <94423959+wuminqaq@users.noreply.github.com>
2025-01-14 15:24:22 +0800

    Update test_threshold_matrix.sh

--- evaluation/script/test_threshold_matrix.sh
+++ evaluation/script/test_threshold_matrix.sh
@@ -9,13 +9,13 @@ if [ $? -eq 0 ];then
     echo "" > $Source
     echo "" > $Source1
     for Critical_bin_id in "${Critical_bin_id_list[@]}"; do
-        sed -i "s/#define Critical_bin_id .*/#define Critical_bin_id ${Critical_bin_id}/" NHC_spgemm.h
+        sed -i "s/#define Critical_bin_id .*/#define Critical_bin_id ${Critical_bin_id}/" ../../inc/NHC_spgemm.h
         for Cnnz_ctile_rate_Threshold in "${Cnnz_ctile_rate_Threshold_list[@]}"; do
             echo "Critical_bin_id is $Critical_bin_id, Cnnz_ctile_rate_Threshold is $Cnnz_ctile_rate_Threshold ----------------------" >> merged_file.csv
             echo "" > ../../data/NHC_4080S_result.csv
             echo "Critical_bin_id is $Critical_bin_id, Cnnz_ctile_rate_Threshold is $Cnnz_ctile_rate_Threshold ----------------------" >> $Source
             echo “Critical_bin_id is $Critical_bin_id, "Cnnz_ctile_rate_Threshold is $Cnnz_ctile_rate_Threshold-----------------------" >> $Source1
-            sed -i "s/^#define Cnnz_ctile_rate_Threshold [0-9]*/#define Cnnz_ctile_rate_Threshold ${Cnnz_ctile_rate_Threshold}/" NHC_spgemm.h
+            sed -i "s/^#define Cnnz_ctile_rate_Threshold [0-9]*/#define Cnnz_ctile_rate_Threshold ${Cnnz_ctile_rate_Threshold}/" ../../inc/NHC_spgemm.h
             #nvcc  -arch=compute_86 -code=sm_86 -O3 -Xcompiler -lrt -lcusparse -I /usr/local/cuda-11.4/include/cub test.cu -o test
             make
             for filename in $filenames; do
